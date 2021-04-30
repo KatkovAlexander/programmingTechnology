c0dab56d1b50448c1cabac1aa8496431575ebaa6
Alexander Katkov <katkov.as@students.dvfu.ru>
2021-04-30 17:41:02 +1000

    added shared_timed_mutex, edited Iterator private var und Iterator constructor, avl tree constructor with parametrs modified, deleted const begin und const end in avl tree methods, added method size in avl tree

--- avlTree/avlTree/avl_tree.h
+++ avlTree/avlTree/avl_tree.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <cstddef>
+#include <shared_mutex>
 #include "smart_pointer.h"
 
 using smart_pointer::smartPointer;
+using std::shared_timed_mutex;
+using std::unique_lock;
+using std::shared_lock;
 
 template<typename key_type, typename value_type>
 class avl_tree
@@ -29,15 +33,16 @@ class avl_tree
     
     using nodepntr = smartPointer<node>;
     nodepntr _tree;
-    size_t _size;
+    size_t _size = 0;
+    mutable shared_timed_mutex _mutex;
     
     typedef class Iterator
     {
         nodepntr _node;
-        nodepntr _tree;
+        avl_tree& _tree;
         
     public:
-        explicit Iterator(nodepntr node = nullptr, nodepntr tree = nullptr)
+        explicit Iterator(avl_tree& tree, nodepntr node = nullptr)
         : _node(node), _tree(tree)
         {}
                 
@@ -59,21 +64,25 @@ class avl_tree
         
         value_type& value()
         {
+            shared_lock<shared_timed_mutex> lock(_tree._mutex);
             return _node -> _value;
         }
         
         value_type& operator*()
         {
+            shared_lock<shared_timed_mutex> lock(_tree._mutex);
             return value();
         }
         
         key_type& key()
         {
+            shared_lock<shared_timed_mutex> lock(_tree._mutex);
             return _node -> _key;
         }
         
         Iterator operator++()
         {
+            unique_lock<shared_timed_mutex> lock(_tree._mutex);
             if(!_node){
                 return *this;
             }
@@ -87,7 +96,7 @@ class avl_tree
                 
             } else {
                 
-                nodepntr q = _tree-> _left; // get start node
+                nodepntr q = _tree._tree-> _left; // get start node
                 nodepntr l;
 
                 while (q) {
@@ -107,7 +116,7 @@ class avl_tree
             }
         }
         
-        Iterator operator++(int)
+        const Iterator operator++(int)
         {
             Iterator tmp = *this;
             ++(*this);
@@ -116,6 +125,7 @@ class avl_tree
         
         Iterator operator--()
         {
+            unique_lock<shared_timed_mutex> lock(_tree._mutex);
             if(!_node) {
                 return *this;
             }
@@ -127,7 +137,7 @@ class avl_tree
                 return *this;
             }
             else {
-                nodepntr q = _tree -> _left; // get start node
+                nodepntr q = _tree._tree -> _left; // get start node
                 nodepntr l;
 
                 while (q) {
@@ -147,7 +157,7 @@ class avl_tree
             }
         }
         
-        Iterator operator--(int)
+        const Iterator operator--(int)
         {
             Iterator tmp = *this;
             --(*this);
@@ -160,13 +170,12 @@ class avl_tree
     
 public:
     
-    typedef Iterator iterator;
     avl_tree() :
     _tree(new node(key_type(), value_type())),
     _size(0)
     {}
     
-    avl_tree(const avl_tree& tree): avl_tree()
+    avl_tree(avl_tree& tree): avl_tree()
     {
         auto iter = tree.begin();
         auto end = tree.end();
@@ -178,54 +187,55 @@ public:
     
     Iterator begin()
     {
-        return Iterator(findMin(_tree -> _left), _tree);
-    }
-    
-    Iterator begin() const
-    {
-        return Iterator(findMin(_tree -> _left), _tree);
+        return Iterator(*this, findMin(_tree -> _left));
     }
     
     Iterator end()
     {
-        return Iterator(nodepntr(nullptr), _tree -> _left);
+        return Iterator(*this, nodepntr(nullptr));
     }
     
-    Iterator end() const
+    size_t size()
     {
-        return Iterator(nodepntr(nullptr), _tree -> _left);
+        shared_lock<shared_timed_mutex> lock(_mutex);
+        return _size();
     }
-
+    
     bool empty() const
     {
+        shared_lock<shared_timed_mutex> lock(_mutex);
         return _size == static_cast<size_t>(0);
     }
     
     void clear()
     {
+        unique_lock<shared_timed_mutex> lock(_mutex);
         _size = 0U;
         _tree -> _left = NULL;
     }
     
     Iterator insert(const key_type& key, const value_type& value)
     {
+        unique_lock<shared_timed_mutex> lock(_mutex);
         _tree -> _left = _insert(_tree -> _left, key, value);
         _size++;
-        return Iterator(_find(_tree -> _left, key), _tree);
+        return Iterator(*this, _find(_tree -> _left, key));
     }
     
     void remove(const key_type& key)
     {
+        unique_lock<shared_timed_mutex> lock(_mutex);
         _tree -> _left = _remove(_tree -> _left, key);
         _size--;
     }
     
     Iterator find(const key_type& key)
     {
+        shared_lock<shared_timed_mutex> lock(_mutex);
         if (!_tree -> _left){
             return end();
         }
-        return Iterator(_find(_tree -> _left, key), _tree);
+        return Iterator(*this, _find(_tree -> _left, key));
     }
     
     value_type& operator[](const key_type& key)
